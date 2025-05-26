55145cb976e5792ffeee402e3e0571b10a0169cb
serenarocc <serenarocc@gmail.com>
2025-05-26 16:52:57 +0200

    lab 9 pt9 commit

--- Client/my-app/src/components/AddFilmForm.jsx
+++ Client/my-app/src/components/AddFilmForm.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Col from 'react-bootstrap/Col';
 import Form from 'react-bootstrap/Form';
+import {Alert} from 'react-bootstrap';
 import InputGroup from 'react-bootstrap/InputGroup';
 import Row from 'react-bootstrap/Row';
 import dayjs from 'dayjs';
@@ -18,6 +19,8 @@ function AddFilmForm({ addFilm, editFilm, filmToEdit }) {
     watchDate: filmToEdit? filmToEdit.watchDate?.format("YYYY-MM-DD") : ""
   });
 
+  const [errorMsg, setErrorMsg] = useState('');
+
   const handleSubmit = (event) => {
     event.preventDefault();
     const newFilm = {
@@ -27,13 +30,22 @@ function AddFilmForm({ addFilm, editFilm, filmToEdit }) {
       watchDate: formData.watchDate ? dayjs(formData.watchDate) : undefined,
       rating: parseInt(formData.rating)
     };
+   
+     // Here some data validation can be inserted, if not yet forced with HTML5 attributes on form controls
+     if (newFilm.title.length == 0) {
+      setErrorMsg('Title length cannot be 0');
+    } else if (newFilm.rating < 0 || newFilm.rating > 5) {
+      setErrorMsg('Invalid value for Rating');
+    }
     isEdit ? editFilm(newFilm) : addFilm(newFilm);
     setValidated(true);
   };
 
 
   return (
-    <Form noValidate validated={validated} onSubmit={handleSubmit}>
+    <>
+     {errorMsg? <Alert variant='danger' onClose={()=>setErrorMsg('')} dismissible>{errorMsg}</Alert> : false }
+    <Form onSubmit={handleSubmit}>
       <Row className="mb-3">
         <Form.Group as={Col} md="6" controlId="validationCustom01">
           <Form.Label>Title</Form.Label>
@@ -88,6 +100,8 @@ function AddFilmForm({ addFilm, editFilm, filmToEdit }) {
 
       <Button type="submit">Submit</Button>
     </Form>
+    </>
+   
   );
 }
 
