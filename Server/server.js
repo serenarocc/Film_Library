949a3632e789a49f953f36460430488d25cfc121
serenarocc <serenarocc@gmail.com>
2025-05-28 18:05:41 +0200

    lab11 pt8 ommit

--- Server/server.js
+++ Server/server.js
@@ -15,7 +15,7 @@ const app = express(); //oggetto server
 app.use(morgan('dev'));
 app.use(express.json()); // abilita parsing JSON
 app.use(cors()); 
-const PORT = 3000;
+const PORT = 3001;
 
 /*** Utility Functions ***/
 
